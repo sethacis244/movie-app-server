be645b9061620db47c87f1c96276a3bf11b8fa3e
Maxim <maximtsoy2001@gmail.com>
2023-09-06 22:48:41 +0600

    added images

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000; // Define the port to listen on
 // Serve static files from the 'public' directory
 app.use('/images/liveshow', express.static(path.join(__dirname, 'public', 'liveshow')));
 app.use('/images/most-popular', express.static(path.join(__dirname, 'public', 'most-popular')));
+app.use('/images/movies-for-you', express.static(path.join(__dirname, 'public', 'movies-for-you')));
+app.use('/images/bangla-music', express.static(path.join(__dirname, 'public', 'bangla-music')));
 
 app.get('/', (req, res) => {
   return res.status(200).json({
